e7391b53a276e939be0e5a9008dfd1d5ce685704
thanmay <thanmaym0804@gmail.com>
2026-08-17 11:40:52 +0530

    Fix MissingPluginException by forcing manual plugin registration in MainActivity.kt

--- mobile_app/android/app/src/main/kotlin/com/spatialeye/navigation/MainActivity.kt
+++ mobile_app/android/app/src/main/kotlin/com/spatialeye/navigation/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.spatialeye.navigation
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
